a356dfa0bd4ab1a248c58121f385337a3189aa89
sun2598 <cksun2598@gmail.com>
2022-12-29 00:36:47 +0900

    DB와 일치하게 reviewImageVo 필드 순서 수정

--- palette/src/kh/s13/palette/review/model/vo/ReviewImageVo.java
+++ palette/src/kh/s13/palette/review/model/vo/ReviewImageVo.java
@@ -2,43 +2,36 @@ package kh.s13.palette.review.model.vo;
 
 public class ReviewImageVo {
 
-//	RFILEPATH NOT NULL VARCHAR2(500) 
 //	RNO       NOT NULL NUMBER  
+//	RFILEPATH NOT NULL VARCHAR2(500) 
 	
-	private String rfilepath;
 	private int rno;
+	private String rfilepath;
 	
 	
 	public ReviewImageVo() {
 		super();
 	}
-
-	public ReviewImageVo(String rfilepath, int rno) {
+	public ReviewImageVo(int rno, String rfilepath) {
 		super();
-		this.rfilepath = rfilepath;
 		this.rno = rno;
+		this.rfilepath = rfilepath;
 	}
-
 	@Override
 	public String toString() {
-		return "ReviewImageVo [rfilepath=" + rfilepath + ", rno=" + rno + "]";
+		return "ReviewImageVo [rno=" + rno + ", rfilepath=" + rfilepath + "]";
 	}
-
-	public String getRfilepath() {
-		return rfilepath;
-	}
-
-	public void setRfilepath(String rfilepath) {
-		this.rfilepath = rfilepath;
-	}
-
 	public int getRno() {
 		return rno;
 	}
-
 	public void setRno(int rno) {
 		this.rno = rno;
 	}
-	
+	public String getRfilepath() {
+		return rfilepath;
+	}
+	public void setRfilepath(String rfilepath) {
+		this.rfilepath = rfilepath;
+	}
 	
 }
